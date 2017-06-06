7adc02677b4dded3b792a30156f805a6514fbec5
Carlos Bruno <cbcarlos08@gmail.com>
2017-06-06 15:53:54 -0400

    Curso de Android -  Envio de SMS

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/LoginActivity.java
@@ -1,8 +1,13 @@
 package br.com.whatsappandroid.cursoandroid.whatsapp.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.support.annotation.StringDef;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +20,7 @@ import java.util.HashMap;
 import java.util.Random;
 
 import br.com.whatsappandroid.cursoandroid.whatsapp.R;
+import br.com.whatsappandroid.cursoandroid.whatsapp.helper.Permissao;
 import br.com.whatsappandroid.cursoandroid.whatsapp.helper.Preferencias;
 
 public class LoginActivity extends AppCompatActivity {
@@ -23,11 +29,17 @@ public class LoginActivity extends AppCompatActivity {
     private EditText codPais;
     private EditText nome;
     private Button   cadatrar;
+    private String[] permissoesNecessarias = new String[]{
+        Manifest.permission.SEND_SMS,
+       Manifest.permission.INTERNET
 
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Permissao.validaPermissoes(1, this, permissoesNecessarias );
 
         telefone = (EditText)  findViewById(R.id.edit_telefone);
         codArea  = (EditText)  findViewById(R.id.edit_cod_area);
@@ -61,14 +73,20 @@ public class LoginActivity extends AppCompatActivity {
                 //GerarToken
                 Random randomico = new Random();
                 int numeroRandomico = randomico.nextInt( 9999 - 1000 ) + 1000;
-
                 String token = String.valueOf( numeroRandomico );
+                String mensagemEnvio = "WhatsApp Código de Confirmação: "+ token;
+
+
 
                 //salvar dados para validação
                 Preferencias preferencias = new Preferencias( LoginActivity.this );
                 preferencias.salvarUsuarioPreferencias(nomeUsuario, telefoneSemFormatacao, token);
 
-                HashMap<String, String> usuario = preferencias.getDadosUsuario();
+                //Envio do SMS
+                boolean enviadoSMS = enviaSMS( "+"+telefoneSemFormatacao, mensagemEnvio );
+
+
+                //HashMap<String, String> usuario = preferencias.getDadosUsuario();
 
 
 
@@ -76,5 +94,50 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+    }
+    private boolean enviaSMS(String telefone, String mensagem){
+
+        try{
+
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(telefone, null, mensagem, null, null);
+            return true;
+
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+
+        super.onRequestPermissionsResult( requestCode, permissions, grantResults );
+
+        for( int resultado : grantResults ){
+
+            if( resultado == PackageManager.PERMISSION_DENIED ){
+
+                alertaValidacaoPermissao();
+
+            }
+
+        }
+    }
+
+    private void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder( this );
+        builder.setTitle("Permissões negadas");
+        builder.setMessage("Para utilizar esse app é necessário aceitar as permissões");
+
+        builder.setPositiveButton("CONFIRMAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
