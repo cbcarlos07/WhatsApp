73263a841aeb514bdbdc9c7e2be3eacf956dd2a5
Carlos Bruno <cbcarlos08@gmail.com>
2017-06-09 09:12:58 -0400

    Curso de Android -  Barra de menu

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.whatsappandroid.cursoandroid.whatsapp.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,12 +20,16 @@ public class MainActivity extends AppCompatActivity {
     //private DatabaseReference referenceFirebase = FirebaseDatabase.getInstance().getReference();
     private Button botaoSair;
     private FirebaseAuth autenticacao;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botaoSair = (Button) findViewById(R.id.bt_sair);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar( toolbar );
+        /*botaoSair = (Button) findViewById(R.id.bt_sair);
 
         botaoSair.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,8 +42,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity( intent );
 
             }
-        });
+        });*/
 
       //  referenceFirebase.child("pontos").setValue(100);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
