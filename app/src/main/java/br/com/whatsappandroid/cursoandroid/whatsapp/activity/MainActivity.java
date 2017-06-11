d97bf1c3adf25a5fd2ced248156c29d2ada9a0c8
Carlos Bruno <cbcarlos08@gmail.com>
2017-06-11 08:49:12 -0400

    Curso de Android -  Botoes

--- app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
+++ app/src/main/java/br/com/whatsappandroid/cursoandroid/whatsapp/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,10 +22,13 @@ public class MainActivity extends AppCompatActivity {
     private Button botaoSair;
     private FirebaseAuth autenticacao;
     private Toolbar toolbar;
+    private FirebaseAuth ususarioAutenticacao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ususarioAutenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("WhatsApp");
@@ -52,5 +56,27 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch ( item.getItemId() ){
+            case R.id.item_sair:
+                deslogarUsuario();
+                return true;
+            case  R.id.item_configuracoes:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    public void deslogarUsuario(){
+        ususarioAutenticacao.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity2.class);
+        startActivity( intent );
+        finish();
     }
 }
